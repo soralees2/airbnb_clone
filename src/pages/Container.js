c287e22da77671e26150036aa5f7cfc3af5104b0
soralees2 <eelsoras2@gmail.com>
2022-05-09 21:48:27 +0900

    global modal markjup

--- src/pages/Container.js
+++ src/pages/Container.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Main from './Main';
+import GlobalModal from './GlobalModal';
 
 const Container = () => {
   return (
     <div className="contents">
+      <GlobalModal />
       <Main />
     </div>
   )
